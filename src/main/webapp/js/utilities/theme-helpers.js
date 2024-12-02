5fbc00d5d173872c647f0620791fbfa1c50930a1
vdbogdanov <v.d.bogdanov@yandex.ru>
2024-12-03 01:33:09 +0300

    Disabling the automatic theme selection feature

--- src/main/webapp/js/utilities/theme-helpers.js
+++ src/main/webapp/js/utilities/theme-helpers.js
@@ -7,15 +7,6 @@ function setTheme(theme) {
 }
 
 function initTheme() {
-  function getBrowserThemePreferences() {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      return "dark";
-    }
-    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-      return "light";
-    }
-    return undefined;
-  }
   function changeEditorThemeSettingIfNecessary(theme) {
     if (theme === "dark" && document.appConfig.editorCreateOptions.theme === "vs") {
       document.appConfig.editorCreateOptions.theme = "vs-dark";
@@ -24,16 +15,8 @@ function initTheme() {
       document.appConfig.editorCreateOptions.theme = "vs";
     }
   }
-  function onMediaColorPreferencesChanged(event) {
-    const theme = event.matches ? "dark" : "light";
-    document.appConfig.theme = theme
-    changeEditorThemeSettingIfNecessary(theme);
-    updateConfig(document.appConfig);
-  }
   // set theme to last saved settings or browser preference or "light"
-  document.appConfig.theme = document.appConfig.theme || getBrowserThemePreferences() || "light";
+  document.appConfig.theme = document.appConfig.theme || "light";
   setTheme(document.appConfig.theme);
   changeEditorThemeSettingIfNecessary(document.appConfig.theme);
-  // listen to browser change event
-  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", onMediaColorPreferencesChanged);
 }
